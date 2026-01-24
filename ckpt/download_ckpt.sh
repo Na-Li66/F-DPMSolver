b2a9b73df4585f2d80472853641706a9484f8214
Na Li <113565640+Na-Li66@users.noreply.github.com>
2026-01-25 08:09:11 +1100

    Update download_ckpt.sh
    Change directory to 'ckpt' before unzipping and rename the extracted model files.

--- ckpt/download_ckpt.sh
+++ ckpt/download_ckpt.sh
@@ -2,5 +2,6 @@
 wget -O ckpt/ffhq-256.zip https://ommer-lab.com/files/latent-diffusion/ffhq.zip
 wget -O ckpt/lsun_beds-256.zip https://ommer-lab.com/files/latent-diffusion/lsun_bedrooms.zip
 
-unzip -o ffhq-256.zip
-unzip -o lsun_beds-256.zip
+cd ckpt
+unzip -o ffhq-256.zip && mv model.ckpt model_ffhq.ckpt
+unzip -o lsun_beds-256.zip && mv model.ckpt model_lsun.ckpt
