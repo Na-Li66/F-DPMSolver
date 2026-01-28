2ba4aa6b23c3f2b9f79ec364b66a8aead7c0581e
Na Li <113565640+Na-Li66@users.noreply.github.com>
2026-01-28 21:14:02 +1100

    Update calculate_metrics_func.py
    Removed commented-out DINOv2Detector class and its related code.

--- fid/calculate_metrics_func.py
+++ fid/calculate_metrics_func.py
@@ -50,40 +50,6 @@ class InceptionV3Detector(Detector):
         return self.model.to(x.device)(x, return_features=True)
 
 #----------------------------------------------------------------------------
-# DINOv2 feature detector.
-# Modeled after https://github.com/layer6ai-labs/dgm-eval
-
-# class DINOv2Detector(Detector):
-#     def __init__(self, resize_mode='torch'):
-#         super().__init__(feature_dim=1024)
-#         self.resize_mode = resize_mode
-#         import warnings
-#         warnings.filterwarnings('ignore', 'xFormers is not available')
-#         torch.hub.set_dir(dnnlib.make_cache_dir_path('torch_hub'))
-#         self.model = torch.hub.load('facebookresearch/dinov2:main', 'dinov2_vitl14', trust_repo=True, verbose=False, skip_validation=True)
-#         self.model.eval().requires_grad_(False)
-
-#     def __call__(self, x):
-#         # Resize images.
-#         if self.resize_mode == 'pil': # Slow reference implementation that matches the original dgm-eval codebase exactly.
-#             device = x.device
-#             x = x.to(torch.uint8).permute(0, 2, 3, 1).cpu().numpy()
-#             x = np.stack([np.uint8(PIL.Image.fromarray(xx, 'RGB').resize((224, 224), PIL.Image.Resampling.BICUBIC)) for xx in x])
-#             x = torch.from_numpy(x).permute(0, 3, 1, 2).to(device)
-#         elif self.resize_mode == 'torch': # Fast practical implementation that yields almost the same results.
-#             x = torch.nn.functional.interpolate(x.to(torch.float32), size=(224, 224), mode='bicubic', antialias=True)
-#         else:
-#             raise ValueError(f'Invalid resize mode "{self.resize_mode}"')
-
-#         # Adjust dynamic range.
-#         x = x.to(torch.float32) / 255
-#         x = x - misc.const_like(x, [0.485, 0.456, 0.406]).reshape(1, -1, 1, 1)
-#         x = x / misc.const_like(x, [0.229, 0.224, 0.225]).reshape(1, -1, 1, 1)
-
-#         # Run DINOv2 model.
-#         return self.model.to(x.device)(x)
-
-#----------------------------------------------------------------------------
 # Metric specifications.
 
 metric_specs = {
